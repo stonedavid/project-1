34b5534da3fc014348b4aba4b5fda59f6a63b510
David Stone <davidstone.nyc@gmail.com>
2016-08-18 22:54:02 +0000

    completed server.js

--- server.js
+++ server.js
@@ -1,9 +1,47 @@
 var express = require("express");
+const url = require('url');
 
 var app = express();
+var natTime
+, nixTime
+, dateObj
+, yearString
+, dateString
+, monthString
+, returnObj;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+function unixText(str) {
+    return (!isNaN(str) && str.length > 0);
+}
+
+function parseNatural(str) {
+    var tokens = str.split(" ");
+    var monthString = tokens[0];
+    var dateString = tokens[1].slice(-1) === "," ? tokens[1].slice(0,-1) : tokens[1];
+    var yearString = tokens[2];
+}
+
+function createReturnObject(d) {
+    return { 
+        unix: (d.getTime() / 1000).toString(),
+        natural : d.toDateString()
+    };
+}
+
+app.get('/*', function (req, res) {
+    
+  if (unixText(req.url.slice(1))) {
+      returnObj = createReturnObject(new Date(Number(req.url.slice(1))*1000));
+  } else {
+      try {
+        returnObj = createReturnObject(new Date(decodeURI(req.url.slice(1))));
+      }
+      catch(err) {
+        console.log(decodeURI(req.url.slice(1)));
+        returnObj = { unix: null, natural: null };
+      }
+    }
+  res.end(JSON.stringify(returnObj));
 });
 
 app.listen(8080, function () {
